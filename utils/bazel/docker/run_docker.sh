8044709c572ba89f6f7c54d1f18e1485ed3b4e40
Sambhav Jain <sambhav.jain@getcruise.com>
2024-02-04 03:04:52 -0800

    fix

--- utils/bazel/docker/run_docker.sh
+++ utils/bazel/docker/run_docker.sh
@@ -2,9 +2,13 @@
 
 docker build -f utils/bazel/docker/Dockerfile \
              -t torch-mlir:dev \
+             --build-arg GROUP=$(id -gn) \
+             --build-arg GID=$(id -g) \
+             --build-arg USER=$(id -un) \
+             --build-arg UID=$(id -u) \
              .
 
 docker run -it \
            -v "$(pwd)":"/opt/src/torch-mlir" \
-           -v "${HOME}/.cache/bazel":"/root/.cache/bazel" \
+           -v "${HOME}/.cache/bazel":"${HOME}/.cache/bazel" \
            torch-mlir:dev
